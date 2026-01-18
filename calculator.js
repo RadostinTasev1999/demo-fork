7d22418ae17feecbb413dcf9fbc815cc3343c779
senior9904 <radostin.tasev22@gmail.com>
2026-01-18 10:32:44 +0200

    Create calculator.js

---
+++ calculator.js
@@ -0,0 +1,43 @@
+class Calculator {
+  constructor() {
+    this.result = 0;
+  }
+
+  add(value) {
+    this.result += value;
+    return this;
+  }
+
+  subtract(value) {
+    this.result -= value;
+    return this;
+  }
+
+  multiply(value) {
+    this.result *= value;
+    return this;
+  }
+
+  divide(value) {
+    if (value === 0) {
+      throw new Error("Cannot divide by zero");
+    }
+    this.result /= value;
+    return this;
+  }
+
+  // NEW METHOD
+  power(exponent) {
+    this.result = Math.pow(this.result, exponent);
+    return this;
+  }
+
+  clear() {
+    this.result = 0;
+    return this;
+  }
+
+  getResult() {
+    return this.result;
+  }
+}
